983232d5d71e4831039e70b8addb2b43ec39bf61
Ruthvik Shinde <121545155+ruthvik2424@users.noreply.github.com>
2023-05-12 16:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,37 +11,33 @@ CORS = app
 @app.route('/calculator', methods=['POST'])
 def calculator():
     data = request.get_data()
-    result = type(data)
-    return result
+    num1 = data['num1']
+    num2 = data['num2']
+    choice = data['choice']
+
+
+
+    if choice == "+":
+        r = n1 + n2
+    elif choice == "-":
+        r = n1 - n2
+    elif choice == "*":
+        r = n1 * n2
+    elif choice == "/":
+        r = n1 / n2
+    elif choice == "**":
+        r = n1 ** n2
+    elif choice == "%":
+        r = n1 % n2
+    else:
+        return jsonify({"error": "Invalid operator"})
     
-
-    # num1 = data['num1']
-    # num2 = data['num2']
-    # choice = data['choice']
-
-
-
-    # if choice == "+":
-    #     r = n1 + n2
-    # elif choice == "-":
-    #     r = n1 - n2
-    # elif choice == "*":
-    #     r = n1 * n2
-    # elif choice == "/":
-    #     r = n1 / n2
-    # elif choice == "**":
-    #     r = n1 ** n2
-    # elif choice == "%":
-    #     r = n1 % n2
-    # else:
-    #     return jsonify({"error": "Invalid operator"})
-    
-    # result = {
-    #     "num1": n1,
-    #     "num2": n2,
-    #     "result": r
-    # }
-    # return jsonify({'result': result}), 200, {'Content-Type': 'application/json'}
+    result = {
+        "num1": n1,
+        "num2": n2,
+        "result": r
+    }
+    return jsonify({'result': result}), 200, {'Content-Type': 'application/json'}
 
 
 
